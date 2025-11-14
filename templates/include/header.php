0a50cb6adb6f452b508855a7da60e44872d58bdd
VirtusStack <payalgupta6403@gmail.com>
2025-11-14 11:31:09 +0530

    Update header.php

--- templates/include/header.php
+++ templates/include/header.php
@@ -1,31 +1,22 @@
 <?php
 // templates/include/header.php
+require_once __DIR__ . '/../../config/config.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Online Examination System</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Online Examination System</title>
 
-    <!-- SB Admin 2 CSS -->
-    <link href="<?= BASE_URL ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-    <link href="<?= BASE_URL ?>/assets/css/sb-admin-2.min.css" rel="stylesheet">
+  <!-- FontAwesome Icons -->
+  <link href="<?= BASE_URL ?>/assets/sb2-admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+  <!-- SB Admin 2 Styles -->
+  <link href="<?= BASE_URL ?>/assets/sb2-admin/css/sb-admin-2.min.css" rel="stylesheet">
+
+  <!-- Custom Styles -->
+  <link href="<?= BASE_URL ?>/assets/sb2-admin/css/custom.css" rel="stylesheet">
+
 </head>
 <body id="page-top">
-
-<!-- Page Wrapper -->
-<div id="wrapper">
-
-    <!-- Sidebar -->
-    <?php include TEMPLATE_PATH . '/include/sidebar.php'; ?>
-    <!-- End of Sidebar -->
-
-    <!-- Content Wrapper -->
-    <div id="content-wrapper" class="d-flex flex-column">
-        <div id="content">
-
-            <!-- Topbar -->
-            <?php include TEMPLATE_PATH . '/include/navbar.php'; ?>
-            <!-- End of Topbar -->
-
-            <div class="container-fluid">
