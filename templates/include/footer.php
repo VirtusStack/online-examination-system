0611efd08b90acc746672193f265bbb3f26dc615
VirtusStack <payalgupta6403@gmail.com>
2025-11-17 11:15:40 +0530

    Update footer.php

--- templates/include/footer.php
+++ templates/include/footer.php
@@ -14,17 +14,15 @@
 </a>
 
 <!-- jQuery and Bootstrap core JavaScript -->
-<script src="<?= BASE_URL ?>assets/vendor/jquery/jquery.min.js"></script>
-<script src="<?= BASE_URL ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= BASE_URL ?>/assets/vendor/jquery/jquery.min.js"></script>
+<script src="<?= BASE_URL ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript -->
-<script src="<?= BASE_URL ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="<?= BASE_URL ?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- SB Admin 2 JavaScript -->
-<script src="<?= BASE_URL ?>assets/js/sb-admin-2.min.js"></script>
+<script src="<?= BASE_URL ?>/assets/js/sb-admin-2.min.js"></script>
 
-<!-- Custom JS -->
-<script src="<?= BASE_URL ?>assets/js/custom.js"></script>
 </body>
 </html>
 
