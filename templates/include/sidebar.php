b8eecd9beb0332487c7881b35e6b98fdb35162db
VirtusStack <payalgupta6403@gmail.com>
2025-11-17 11:15:44 +0530

    Update sidebar.php

--- templates/include/sidebar.php
+++ templates/include/sidebar.php
@@ -2,11 +2,10 @@
 // templates/include/sidebar.php
 ?>
 
-<!-- Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="admin.php?action=dashboard">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= BASE_URL ?>/admin.php?action=dashboard">
         <div class="sidebar-brand-icon">
             <i class="fas fa-graduation-cap"></i>
         </div>
@@ -15,43 +14,80 @@
 
     <hr class="sidebar-divider my-0">
 
-    <!-- Dashboard Link -->
+    <!-- Dashboard -->
     <li class="nav-item">
-        <a class="nav-link" href="admin.php?action=dashboard">
+        <a class="nav-link" href="<?= BASE_URL ?>/admin.php?action=dashboard">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
+            <span>Dashboard</span>
+        </a>
     </li>
 
     <hr class="sidebar-divider">
 
     <!-- Subjects -->
     <li class="nav-item">
-        <a class="nav-link" href="admin.php?action=manageSubjects">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSubjects">
             <i class="fas fa-fw fa-book"></i>
-            <span>Subjects</span></a>
+            <span>Subjects</span>
+        </a>
+        <div id="collapseSubjects" class="collapse" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=manageSubjects">Manage Subjects</a>
+                <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=newSubject">Add Subject</a>
+            </div>
+        </div>
     </li>
 
-    <!-- Exams -->
+    <!-- Question Banks -->
     <li class="nav-item">
-        <a class="nav-link" href="admin.php?action=manageExams">
-            <i class="fas fa-fw fa-file-alt"></i>
-            <span>Exams</span></a>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBanks">
+            <i class="fas fa-fw fa-layer-group"></i>
+            <span>Question Banks</span>
+        </a>
+        <div id="collapseBanks" class="collapse" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=manageBanks">Manage Banks</a>
+                <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=newBank">Add Bank</a>
+            </div>
+        </div>
     </li>
 
     <!-- Questions -->
     <li class="nav-item">
-        <a class="nav-link" href="admin.php?action=manageQuestions">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseQuestions">
             <i class="fas fa-fw fa-question-circle"></i>
-            <span>Questions</span></a>
+            <span>Questions</span>
+        </a>
+        <div id="collapseQuestions" class="collapse" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=manageQuestions">Manage Questions</a>
+                <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=newQuestion">Add Question</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Exams -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseExams">
+            <i class="fas fa-fw fa-file-alt"></i>
+            <span>Exams</span>
+        </a>
+        <div id="collapseExams" class="collapse" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=manageExams">Manage Exams</a>
+                <a class="collapse-item" href="<?= BASE_URL ?>/admin.php?action=newExam">Create Exam</a>
+            </div>
+        </div>
     </li>
 
     <hr class="sidebar-divider d-none d-md-block">
 
     <!-- Logout -->
     <li class="nav-item">
-        <a class="nav-link" href="admin.php?action=logout">
+        <a class="nav-link" href="<?= BASE_URL ?>/admin.php?action=logout">
             <i class="fas fa-fw fa-sign-out-alt"></i>
-            <span>Logout</span></a>
+            <span>Logout</span>
+        </a>
     </li>
 
 </ul>
