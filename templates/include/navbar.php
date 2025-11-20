b5e8fef4afd6d40a642cbf42c5de3bf22ab0a431
VirtusStack <payalgupta6403@gmail.com>
2025-11-20 11:02:37 +0530

    Update navbar.php

--- templates/include/navbar.php
+++ templates/include/navbar.php
@@ -1,6 +1,9 @@
 <?php
-// templates/include/navbar.php
+// /templates/include/navbar.php
+// Admin navbar - Online Examination System
+$adminName = $_SESSION['admin_name'] ?? 'Admin';
 ?>
+
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
     <!-- Sidebar Toggle (Topbar) -->
@@ -8,36 +11,26 @@
         <i class="fa fa-bars"></i>
     </button>
 
-    <!-- Page Title -->
-    <h5 class="text-primary font-weight-bold mb-0">Online Examination System</h5>
-
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
 
-        <!-- Divider -->
-        <div class="topbar-divider d-none d-sm-block"></div>
-
-        <!-- User Information -->
+        <!-- Admin Info -->
         <li class="nav-item dropdown no-arrow">
-            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-                    Admin
-                </span>
-                <img class="img-profile rounded-circle" src="<?= BASE_URL ?>/assets/img/undraw_profile_1.svg" alt="Profile">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= htmlspecialchars($adminName) ?></span>
+                <i class="fas fa-user-circle fa-lg"></i>
             </a>
 
-            <!-- Dropdown -->
+            <!-- Dropdown - Logout -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Profile
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item text-danger" href="<?= BASE_URL ?>/templates/common/logout.php">
-                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-danger"></i>
+                <a class="dropdown-item" href="admin.php?action=logout">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Logout
                 </a>
             </div>
         </li>
+
     </ul>
+
 </nav>
