8307ba9a715c473d91f9d80f08c0c7598f524907
VirtusStack <payalgupta6403@gmail.com>
2025-11-20 12:58:28 +0530

    Update online_exam_system.sql

--- online_exam_system.sql
+++ online_exam_system.sql
@@ -37,16 +37,6 @@ CREATE TABLE `exams` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `exams`
---
-
-INSERT INTO `exams` (`exam_id`, `subject_id`, `exam_title`, `duration_minutes`, `total_marks`, `start_date`, `end_date`, `status`, `created_at`) VALUES
-(1, 1, 'Mathematics Exam 1', 30, 5.00, '2025-11-15 11:00:00', '2025-11-15 11:30:00', 'Active', '2025-11-17 10:15:42'),
-(2, 2, 'Physics Exam 1', 30, 4.00, '2025-11-21 11:00:00', '2025-11-21 11:30:00', 'Active', '2025-11-17 11:35:32'),
-(3, 2, 'Physics Exam 2', 30, 4.00, '2025-11-21 11:00:00', '2025-11-21 11:30:00', 'Active', '2025-11-17 11:35:44'),
-(4, 5, 'Computer Science Exam 1', 30, 30.00, '2025-11-21 11:00:00', '2025-11-21 11:30:00', 'Active', '2025-11-17 13:20:15');
-
 -- --------------------------------------------------------
 
 --
@@ -58,20 +48,6 @@ CREATE TABLE `exam_questions` (
   `exam_id` int(11) NOT NULL,
   `question_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `exam_questions`
---
-
-INSERT INTO `exam_questions` (`id`, `exam_id`, `question_id`) VALUES
-(22, 4, 8),
-(23, 1, 2),
-(24, 1, 1),
-(28, 2, 7),
-(29, 2, 6),
-(30, 2, 5),
-(31, 3, 5),
-(32, 3, 4);
 
 -- --------------------------------------------------------
 
@@ -94,21 +70,6 @@ CREATE TABLE `questions` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `questions`
---
-
-INSERT INTO `questions` (`question_id`, `bank_id`, `question_text`, `option_a`, `option_b`, `option_c`, `option_d`, `correct_option`, `marks`, `negative_marks`, `difficulty`, `created_at`) VALUES
-(1, 1, 'Solve 2x + 3 = 7. Find x.', '1', '2', '3', '4', 'A', 1.00, 0.00, 'Easy', '2025-11-17 10:09:13'),
-(2, 1, 'What is (x+2)(x-3)?', 'x^2 - x - 6', 'x^2 + x - 6', 'x^2 - 6', 'x^2 + 6', 'A', 1.00, 0.00, 'Medium', '2025-11-15 01:40:00'),
-(3, 2, 'Sum of angles in a triangle?', '90', '180', '360', '270', 'B', 1.00, 0.00, 'Easy', '2025-11-15 01:50:00'),
-(4, 2, 'Area of circle formula?', 'πr^2', '2πr', 'πd', 'πr', 'A', 1.00, 0.00, 'Easy', '2025-11-15 02:00:00'),
-(5, 3, 'Newton’s second law?', 'F = ma', 'E = mc^2', 'P = mv', 'V = IR', 'A', 1.00, 0.00, 'Easy', '2025-11-15 02:10:00'),
-(6, 3, 'Unit of force?', 'Newton', 'Joule', 'Watt', 'Pascal', 'A', 1.00, 0.00, 'Easy', '2025-11-15 02:20:00'),
-(7, 4, 'Speed of light in vacuum?', '3×10^8 m/s', '1.5×10^8 m/s', '3×10^6 m/s', '3×10^9 m/s', 'A', 1.00, 0.00, 'Easy', '2025-11-15 02:30:00'),
-(8, 5, 'Which language is used for web development?', 'Python', 'HTML', 'C++', 'Java', 'B', 1.00, 0.00, 'Easy', '2025-11-15 02:40:00'),
-(9, 5, 'What is the output of print(2 + 3)?', '23', '5', '2+3', 'Error', 'B', 1.00, 0.00, 'Easy', '2025-11-15 02:50:00');
-
 -- --------------------------------------------------------
 
 --
@@ -122,17 +83,6 @@ CREATE TABLE `question_banks` (
   `description` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `question_banks`
---
-
-INSERT INTO `question_banks` (`bank_id`, `subject_id`, `bank_name`, `description`, `created_at`) VALUES
-(1, 1, 'Algebra Bank', 'Questions related to algebra', '2025-11-17 10:03:41'),
-(2, 1, 'Geometry Bank', 'Questions related to geometry', '2025-11-17 10:05:08'),
-(3, 2, 'Mechanics Bank', 'Questions on motion and forces', '2025-11-17 10:06:09'),
-(4, 2, 'Optics Bank', 'Questions on light and optics', 2025-11-17 10:08:14'),
-(5, 5, 'Programming Bank', 'Basic programming questions', '2025-11-17 10:20:08');
 
 -- --------------------------------------------------------
 
@@ -152,7 +102,7 @@ CREATE TABLE `subjects` (
 --
 
 INSERT INTO `subjects` (`subject_id`, `subject_name`, `description`, `created_at`) VALUES
-(1, 'Mathematics', 'Covers algebra, geometry, trigonometry, and basic calculus\'', '2025-11-15 04:30:00'),
+(1, 'Mathematics', 'Covers algebra, geometry, trigonometry, and basic calculus/', '2025-11-15 04:30:00'),
 (2, 'Physics', 'Covers motion, heat, light, and electricity', '2025-11-15 04:40:00'),
 (3, 'Chemistry', 'Covers organic, inorganic, and physical chemistry', '2025-11-15 04:50:00'),
 (4, 'Biology', 'Covers cell biology, genetics, human body.', '2025-11-15 05:00:00'),
@@ -219,25 +169,25 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `exams`
 --
 ALTER TABLE `exams`
-  MODIFY `exam_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `exam_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `exam_questions`
 --
 ALTER TABLE `exam_questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `question_banks`
 --
 ALTER TABLE `question_banks`
-  MODIFY `bank_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `bank_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `subjects`
@@ -245,6 +195,12 @@ ALTER TABLE `question_banks`
 ALTER TABLE `subjects`
   MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `exams`
 --
 ALTER TABLE `exams`
   ADD CONSTRAINT `exams_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`subject_id`) ON DELETE CASCADE;
