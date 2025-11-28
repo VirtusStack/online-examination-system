f9024089defc435389f8da27b2dacb1fb3534b24
VirtusStack <payalgupta6403@gmail.com>
2025-11-28 19:42:50 +0530

    Create footer.php

---
+++ templates/student/footer.php
@@ -0,0 +1,28 @@
+  <!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Online Examination System <?= date('Y') ?></span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- jQuery and Bootstrap core JavaScript -->
+<script src="<?= BASE_URL ?>/assets/vendor/jquery/jquery.min.js"></script>
+<script src="<?= BASE_URL ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript -->
+<script src="<?= BASE_URL ?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- SB Admin 2 JavaScript -->
+<script src="<?= BASE_URL ?>/assets/js/sb-admin-2.min.js"></script>
+
+</body>
+</html>
+
