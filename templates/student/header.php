d2f2899207ad06473e0a7503c7f02aed97d4774c
VirtusStack <payalgupta6403@gmail.com>
2025-11-28 19:42:46 +0530

    Update header.php

--- templates/student/header.php
+++ templates/student/header.php
@@ -1,16 +1,20 @@
+<?php
+// templates/include/header.php
+require_once __DIR__ . '/../../config/config.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title><?= $results['pageTitle'] ?? 'Student Panel' ?></title>
-<link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-<link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+ <!-- FontAwesome Icons -->
+  <link href="<?= BASE_URL ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+  <!-- SB Admin 2 Styles -->
+  <link href="<?= BASE_URL ?>/assets/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
+
 <body id="page-top">
 <div id="wrapper">
-<?php include __DIR__ . "/sidebar.php"; ?>
-<div id="content-wrapper" class="d-flex flex-column">
-<div id="content">
-<?php include __DIR__ . "/topbar.php"; ?>
-<div class="container-fluid">
+
