d112b4d885ceee7a0a1e0da895e0611c1bd8c8fb
VirtusStack <payalgupta6403@gmail.com>
2025-11-28 15:50:24 +0530

    Create sidebar.php

---
+++ templates/student/sidebar.php
@@ -0,0 +1,34 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="student.php?action=dashboard">
+        <div class="sidebar-brand-text mx-3">Student Panel</div>
+    </a>
+    <hr class="sidebar-divider my-0">
+
+    <li class="nav-item">
+        <a class="nav-link" href="student.php?action=dashboard">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="student.php?action=exams">
+            <i class="fas fa-fw fa-book"></i>
+            <span>My Exams</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="student.php?action=results">
+            <i class="fas fa-fw fa-chart-bar"></i>
+            <span>Results</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="student.php?action=logout">
+            <i class="fas fa-fw fa-sign-out-alt"></i>
+            <span>Logout</span>
+        </a>
+    </li>
+</ul>
